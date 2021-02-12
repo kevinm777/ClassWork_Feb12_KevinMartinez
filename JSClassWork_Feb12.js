14b73a6d7b452a63f412be271dfcaad69adb21b8
Kevin Martinez Palacios <69471354+kevinm777@users.noreply.github.com>
2021-02-12 10:00:10 -0600

    Add files via upload

---
+++ JSClassWork_Feb12.js
@@ -0,0 +1,27 @@
+//lets create a calculator
+
+function add(a,b)
+{
+let sum = a+b;
+console.log("The add function result is "+ sum);
+}
+function sub(a,b)
+{
+let diff = a-b;
+console.log("The add function result is "+ diff);
+}
+function mul(a,b)
+{
+let product = a*b;
+console.log("The add function result is "+ product);
+}
+function div(a,b)
+{
+let div = a/b;
+console.log("The add function result is "+ div);
+}
+
+add(1000,67);
+sub(100,-100);
+mul(4,5);
+div(49,7);
